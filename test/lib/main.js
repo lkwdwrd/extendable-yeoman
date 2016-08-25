3388866526a10312bc0a88301574580e639cdcdb
Luke Woodward <woodward.lucas@gmail.com>
2016-08-25 08:19:22 -0500

    Switch back to function keyword for 'this' binding.

--- test/lib/main.js
+++ test/lib/main.js
@@ -5,11 +5,11 @@ import Base from '../../lib/base';
 
 chai.should();
 
-describe('The main object', () => {
-	it('contains a Yeoman reference', () => {
+describe('The main object', function(){
+	it('contains a Yeoman reference', function(){
 		main.Yeoman.should.equal(Yeoman);
 	});
-	it('contains the extended Base object', () => {
+	it('contains the extended Base object', function(){
 		main.Base.should.equal(Base);
 	});
 });
