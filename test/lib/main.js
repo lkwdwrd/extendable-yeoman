50544b2ceafe9d159525884a055d8555b4841d35
Luke Woodward <woodward.lucas@gmail.com>
2016-08-25 08:22:48 -0500

    Switch back to straight assert

--- test/lib/main.js
+++ test/lib/main.js
@@ -1,15 +1,13 @@
-import chai from 'chai';
+import {assert} from 'chai';
 import Yeoman from 'yeoman-generator';
 import main from '../../lib/main';
 import Base from '../../lib/base';
 
-chai.should();
-
 describe('The main object', function(){
 	it('contains a Yeoman reference', function(){
-		main.Yeoman.should.equal(Yeoman);
+		assert.equal(main.Yeoman, Yeoman);
 	});
 	it('contains the extended Base object', function(){
-		main.Base.should.equal(Base);
+		assert.equal(main.Base, Base);
 	});
 });
